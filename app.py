cd79ef93f2cb18ecff6a8a34c12f4f23c5cd58d1
Tejas Udayakumar <tejasu.kumar123@gmail.com>
2019-09-01 11:26:31 +0530

    adding flask app

---
+++ app.py
@@ -0,0 +1,20 @@
+from __future__ import division, print_function
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+# Define a flask app
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    # app.run(port=5002, debug=True)
+
+    # Serve the app with gevent
+    http_server = WSGIServer(('0.0.0.0', 5000), app)
+    http_server.serve_forever()
